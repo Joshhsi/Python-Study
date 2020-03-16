1eaa6a347d675b52929c4bd904cfd859adc99930
quiniechen <32817754+quiniechen@users.noreply.github.com>
2020-03-16 09:28:09 +0800

    Update fill_self_assessment.py

--- Practice/Quinie/fill_self_assessment.py
+++ Practice/Quinie/fill_self_assessment.py
@@ -4,81 +4,46 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
+# 進入Windows HCL
+web = webdriver.Chrome("D:\python\chromedriver.exe") 
+urlwindows='https://familyweb.wistron.com/whrs/login.aspx'
+web.get(urlwindows)
+web.maximize_window()
 
-a = list()
-#針對輸入想查詢的型號關鍵字去各hcl網頁搜尋logo資訊並擷圖
-a.append(input("請輸入查詢關鍵字"))
-while True:
-    b = input()
-    if b == "":
-        break
-    else:
-        a.append(b)
-        # 進入Windows HCL
-        web = webdriver.Chrome("D:\python\chromedriver.exe") 
-        urlwindows='https://www.windowsservercatalog.com/default.aspx'
-        web.maximize_window()
-        
-        #利用填入欲查詢的型號，開始進入搜尋
-        web.get(urlwindows)
-        web.find_element_by_name('text').clear()
-        web.find_element_by_name('text').send_keys(b)
-        time.sleep(4)
-        web.find_element_by_id('searchGo').click()
-        web.find_element_by_class_name('productName').click()
-        
-        osoption = list()
-        s1 = Select(web.find_element_by_id('CcitemDetailsdump2_ddlOSFilter'))
-        
-        #針對有拿認証的版本逐一進去擷圖
-        for select in s1.options:
-            osoption.append(select.text)
+web.find_element_by_name('username').clear()
+web.find_element_by_name('username').send_keys('quinie_chen')
+web.find_element_by_name('userpass').clear()
+web.find_element_by_name('userpass').send_keys('10112008')
+#web.find_element_by_partial_link_text('登入').click()
+web.find_element_by_xpath('//*[@id="loginform"]/form/div/input[3]').click()
+time.sleep(4)
+#web.execute_script("window.alert('請填寫緯創防疫自我檢視填報網站');")    
 
-        for select in osoption:
-            if select == "(select a compatible Windows version)":
-                print('start'+select)
-            else:
-                print('開始印'+select)
-                s2 = Select(web.find_element_by_id('CcitemDetailsdump2_ddlOSFilter'))
-                s2.select_by_visible_text(select)
-                web.find_element_by_id('submissionByOSGo').click()
-                              
-                web.execute_script("""
-                (
-                function () 
-                {
-                var y = 0;
-                var step = 400;
-                window.scroll(0, 0);
-                    function f() 
-                    {
-                        if (y < document.body.scrollHeight) 
-                        {
-                        y  = step;
-                        window.scroll(0, y);
-                        setTimeout(f, 50);
-                    } 
-                    else 
-                    {
-                        window.scroll(0, 0);
-                        document.title  = "scroll-done";
-                    }
-                }
-                setTimeout(f, 1000);
-                }
-                ) 
-                ();""")
-                    
-                time.sleep(2)
-                web.save_screenshot("WindowsHCL_"+ b + '_' + select  + ".png")      
-                   
-        web.close()
+#isChecked = web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[15]')
+#web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[20]').click()
+#isChecked = web.findElement(By.xpath('//*[@id="loginform"]/div/form/input[15]'));
+if web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[16]').is_selected():
+    print ('selected!')
+else:
+    print('選擇的內容是text：')
+    print(web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[16]').text)
+    print('選擇的內容是name：')
+    print(web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[16]').get_attribute('value'))
+    checked_value = web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[16]').get_attribute('value')
+    if (checked_value == '1'):
+        web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[16]').click()
+        web.find_element_by_xpath('//*[@id="loginform"]/div/form/input[21]').click()
+        dialog_box = web.switch_to_alert()
+        '''新增等待時間'''
+        time.sleep(2)
+        '''獲取對話方塊的內容'''
+        #列印警告對話方塊內容
+        print (dialog_box.text)  
+        '''點選【確認】顯示"您為何如此自信？"'''
+        dialog_box.accept()   #接受彈窗
+    else:  print('選擇的內容不是1，取消!')   
 
-
-
-
-
-
+web.close()
 
 
 
