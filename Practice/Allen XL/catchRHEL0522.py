e13665e0576e3d9130a91685301ec16b7290b6b3
alion20 <49772916+alion20@users.noreply.github.com>
2020-05-22 15:18:16 +0800

    Create catchRHEL0522.py

---
+++ Practice/Allen XL/catchRHEL0522.py
@@ -0,0 +1,135 @@
+import urllib
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import numpy as np
+
+
+
+
+
+web2  = requests.get('https://access.redhat.com/ecosystem/hardware/3649241')
+soup = BeautifulSoup( web2.text ,'html.parser')
+#print (soup)
+
+
+
+
+
+print ("==========================================================================================")
+print ("==========================================================================================")
+tableData = soup.find('table',  attrs={'class' :  'table table-cp tablesaw tablesaw-stack' })
+#---------------------------table---------------------------
+#
+#
+# --------------------------<thead>---------------------------
+#   tr  th th ... th                                                                  
+#-----------------------------------------------------------------
+#
+#
+# --------------------------<tbody>---------------------------
+#   tr1  th td ... td     
+#   tr2  th td ... td
+#    .      .     .   ...  .
+#    .      .     .   ...  .
+#    .      .     .   ...  .
+#   trX th td ... td
+#  ---------------------------------------------------------------
+#
+#
+#----------------------------------------------------------------
+
+
+
+rows = list()
+columns = list()
+r= []
+rTotal= []
+
+c = []
+th = []
+cTotal = []
+
+thead = tableData.find('thead')
+# --------------------------<thead>--------------------------- < -----取得thead資料
+#   tr  th th ... th                          
+#-----------------------------------------------------------------
+
+
+
+
+
+
+for theadTh in thead.find_all('th') :
+    r=  [theadTh.text.replace('\n', '').replace('\xa0', '') ]
+    rTotal = rTotal + r
+
+columns = rTotal
+#print (columns)
+
+
+
+tbody = tableData.find('tbody')
+# --------------------------<tbody>---------------------------  < -----只拿body資料      
+#   tr1  th td ... td     
+#   tr2  th td ... td
+#    .      .     .   ...  .
+#    .      .     .   ...  .
+#    .      .     .   ...  .
+#   trX th td ... td
+#  ---------------------------------------------------------------
+
+
+
+for tbodyTr in tbody.find_all('tr') :
+# --------------------------<tbody>---------------------------        
+#   tr1  th td ... td      <----------------用for回圈 依序拿出tr，第一個for回圈拿這個
+#   tr2  th td ... td      <----------------第二個for回圈拿這個
+#    .      .     .   ...  .        <----------------第三個for回圈拿這個
+#    .      .     .   ...  .       <----------------
+#    .      .     .   ...  .       <----------------
+#   trX th td ... td      <----------------第X個for回圈拿這個
+#  ---------------------------------------------------------------
+
+
+
+    for tbodyTrTh in tbodyTr.find_all('th') :
+#  ---------------------------------------------------------------
+#   trX th td td td ... td      <----------------用for回圈 依序從trX裡面依序拿出th 跟 td
+#           ^   ^   ^  ^    ^  ^
+#  ---------------------------------------------------------------
+
+
+
+        tt =  tbodyTrTh.text.replace('\n', '').replace('\xa0', '')
+        tt = tt.strip()
+        th=  [ tt ] 
+        cTotal = cTotal + th
+
+
+
+
+    for tbodyTrTd in tbodyTr.find_all('td') :
+        tt2= tbodyTrTd.text.replace('\n', '').replace('\xa0', '')
+        tt2 = tt2.strip()
+        c=  [tt2] 
+        cTotal = cTotal + c
+
+
+    print (cTotal)
+    #print (len(cTotal))
+    if (len(cTotal) == 6 ):
+        rows.append( cTotal)
+    cTotal = []
+
+#print(rows)
+
+df = pd.DataFrame(data=rows, columns=columns)
+#df.to_excel('table4RHEL.xls')
+
+#print(df)
+
+
